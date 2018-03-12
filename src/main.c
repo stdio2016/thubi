afe0a01d46abfcc46f9f5740a57cf5d416f98fb6
stdio2016 <randomfrank8@gmail.com>
2018-03-12 23:50:52 +0800

    show error message if file cannot load

--- src/main.c
+++ src/main.c
@@ -523,6 +523,7 @@ int main(int argc, char *argv[])
   }
   f = fopen(argv[1], "r");
   if (f == NULL) {
+    fprintf(stderr, "unable to open file %s\n", argv[1]);
     return 1;
   }
   Symbols = Trie_create();
